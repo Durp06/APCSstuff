5d1f13c36f2483e5941205070a09c549e298073c
Durp06 <slyles2006@gmail.com>
2023-03-14 14:18:39 -0400

    experimenting with parents

--- Inherit/src/VehicleTester.java
+++ Inherit/src/VehicleTester.java
@@ -2,7 +2,9 @@ public class VehicleTester {
     public static void main (String[] args){
         Vehicle car = new Vehicle("Honda", "Civic", 2013, 1);
         Motorcycle b = new Motorcycle("Harley", "Sportster", 2023, 1, 1200);
-        b.setYear(2022);
+        Comparable c = new Motorcycle("Toyota", "IG", 2023, 1, 2023);
+        ((Vehicle)c).setYear(2022);
         System.out.print(b);
+        System.out.println(car.compareTo(((Vehicle)c)));
     }
 }
