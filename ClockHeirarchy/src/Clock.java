127e9218bdb6fabdf9da15fc09d0f53cbe269f1b
Durp06 <slyles2006@gmail.com>
2023-03-27 14:29:10 -0400

    started clock, it is broken

---
+++ ClockHeirarchy/src/Clock.java
@@ -0,0 +1,41 @@
+
+public class Clock {
+
+    public Clock(){}
+
+    public int getHours(){
+        String time =  java.time.LocalTime.now().toString();
+        int hours = Integer.parseInt(time.substring(0, 2));
+        if(hours>12){
+            hours = 12;
+        }
+        return hours;
+    }
+
+    public int getMinutes(){
+        String time =  java.time.LocalTime.now().toString();
+        int minutes = Integer.parseInt(time.substring(4, 6));
+        return minutes;
+    }
+
+    public boolean isWrapped(){
+        String time = java.time.LocalTime.now().toString();
+        boolean wrap = false;
+        int hours = Integer.parseInt(time.substring(0, 2));
+        if(hours>12){
+            wrap=true;
+        }
+        return wrap;
+    }
+
+    public String getTime(){
+        String morn = "";
+        if (isWrapped()){
+            morn = "am";
+        }
+        else if (!isWrapped()){
+            morn = "pm";
+        }
+        return "It is " + getHours() + ":" + getMinutes() + morn;
+    }
+}
---
+++ ClockHeirarchy/src/ClockTester.java
@@ -0,0 +1,6 @@
+public class ClockTester {
+    public static void main(String[] args) throws Exception {
+        Clock korben = new Clock ();
+        korben.getTime();
+    }
+}
